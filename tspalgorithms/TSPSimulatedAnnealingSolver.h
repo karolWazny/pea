042eac560aba07b764bad45e561d3af7501d5e4f
karolWazny <63009342+karolWazny@users.noreply.github.com>
2021-11-18 17:45:37 +0100

    created TSPSimulatedAnnealingSolver class

---
+++ tspalgorithms/TSPSimulatedAnnealingSolver.h
@@ -0,0 +1,11 @@
+#ifndef PEA_TSPSIMULATEDANNEALINGSOLVER_H
+#define PEA_TSPSIMULATEDANNEALINGSOLVER_H
+
+#include "TSPAbstractSolver.h"
+
+class TSPSimulatedAnnealingSolver : public TSPAbstractSolver {
+
+};
+
+
+#endif //PEA_TSPSIMULATEDANNEALINGSOLVER_H
