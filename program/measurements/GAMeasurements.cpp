2b93909750dd929bf4f697b92a4a1561326a53ce
karolWazny <63009342+karolWazny@users.noreply.github.com>
2022-01-05 18:44:53 +0100

    defined << operators and file headers

--- program/measurements/GAMeasurements.cpp
+++ program/measurements/GAMeasurements.cpp
@@ -6,6 +6,13 @@
 #include "GAMeasurements.h"
 #include "../../utils/display.h"
 
+const std::string GAMeasurements::headers[6] = {"TIME",
+                                          "INSTANCE",
+                                          "POPULATION",
+                                          "CROSSOVER_RATE",
+                                          "MUTATION_RATE",
+                                          "MUTATION_METHOD"};
+
 void GAMeasurements::runParametrizedMeasurement() {
     auto filename = timeString() + ".txt";
     for(int i = 0; i < filesAmount; i++){
@@ -16,3 +23,28 @@ void GAMeasurements::runParametrizedMeasurement() {
 
     }
 }
+
+std::ostream& operator<<(std::ostream& stream, GAMeasurements& measurements){
+    for(int i = 0; i < 5; i++){
+        stream << GAMeasurements::headers[i] << '\t';
+    }
+    stream << GAMeasurements::headers[5] << std::endl;
+
+    auto iterator = measurements.getMeasuredValues().iterator();
+    while(iterator.hasNext()){
+        stream << iterator.next() << std::endl;
+    }
+
+    return stream;
+}
+
+std::ostream& operator<<(std::ostream& stream, GASingleMeasurement& measurement){
+    auto& params = measurement.parameters;
+    stream << std::to_string(measurement.time) << '\t'
+            << measurement.instanceFilename << '\t'
+            << std::to_string(params.population) << '\t'
+            << std::to_string(params.crossoverProbability) << '\t'
+            << std::to_string(params.mutationProbability) << '\t'
+            << params.mutationMethod;
+    return stream;
+};
--- program/measurements/GAMeasurements.h
+++ program/measurements/GAMeasurements.h
@@ -7,21 +7,33 @@
 
 
 #include "TimeMeasurer.h"
-
-class GAMeasurements : public TimeMeasurer{
-public:
-    GAMeasurements() : displayedStuff(ffstack<std::string>(10)){};
-    void runParametrizedMeasurement();
-private:
-    ffstack<std::string> displayedStuff;
-};
+#include "../../tspalgorithms/third/GeneticSolver.h"
 
 struct GASingleMeasurement{
 public:
     long double time;
     std::string instanceFilename;
+    GeneticSolver::Parameters parameters;
+};
+
+class GAMeasurements : public TimeMeasurer{
+public:
+    GAMeasurements() : displayedStuff(ffstack<std::string>(10)){};
+    void runParametrizedMeasurement();
+    LinkedList<GASingleMeasurement>& getMeasuredValues(){
+        return measurements;
+    }
+
+    static const std::string headers[6];
+private:
+    ffstack<std::string> displayedStuff;
+
+    LinkedList<GASingleMeasurement> measurements;
 
 };
+
+std::ostream& operator<<(std::ostream&, GASingleMeasurement&);
+std::ostream& operator<<(std::ostream&, GAMeasurements&);
 
 
 #endif //PEA_GAMEASUREMENTS_H
