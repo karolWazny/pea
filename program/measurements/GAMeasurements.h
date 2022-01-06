49fa452a899e08f8facdd0415fe7f0c379117aed
karolWazny <63009342+karolWazny@users.noreply.github.com>
2022-01-06 21:38:26 +0100

    new values in crossoverRates and in mutationRates

--- program/measurements/GAMeasurements.h
+++ program/measurements/GAMeasurements.h
@@ -54,8 +54,8 @@ private:
     ffstack<std::string> displayedStuff;
 
     static constexpr int populationSizes[] = {30, 70, 120};
-    static constexpr double crossoverRates[] = {0.0, 0.3, 0.6, 0.8, 1.0};
-    static constexpr double mutationRates[] = {0.0, 0.01, 0.05, 0.1, 0.3, 0.5, 0.8, 1.0};
+    static constexpr double crossoverRates[] = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+    static constexpr double mutationRates[] = {0.01, 0.05, 0.1, 0.2, 0.25, 0.3, 0.5, 0.7};
     static constexpr MutationMethod mutationMethods[] = {MutationMethod::RANDOM, MutationMethod::REVERSE};
 
     LinkedList<GASingleMeasurement> measurements;
